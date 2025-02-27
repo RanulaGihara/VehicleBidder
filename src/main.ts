293c894b2e34f30167429fd77b5fd65a26be7244
ranula.gamage <ranula.g@insighture.com>
2025-02-27 14:29:06 +0530

    Initial commit: VEHICLE BIDDER Vue 3 project with TypeScript and Pinia

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import "./style.css";
+import App from "./App.vue";
+
+const app = createApp(App);
+
+// Create and use Pinia
+const pinia = createPinia();
+app.use(pinia);
+
+app.mount("#app");
