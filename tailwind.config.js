b2d6557e9277bf3fbcf6291b2d14020f3cb532bb
ranula.gamage <ranula.g@insighture.com>
2025-02-28 16:40:36 +0530

    feat: setup tailwind globally

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default  {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["system-ui", "Avenir", "Helvetica", "Arial", "sans-serif"],
+      },
+      colors: {
+        background: {
+          DEFAULT: "#242424", // dark background
+          light: "#ffffff", // light background
+        },
+        text: {
+          DEFAULT: "rgba(255, 255, 255, 0.87)",
+          light: "#213547", // light text color
+        },
+        link: {
+          DEFAULT: "#646cff",
+          hover: "#535bf2",
+          lightHover: "#747bff",
+        },
+        button: {
+          DEFAULT: "#1a1a1a",
+          light: "#f9f9f9",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
