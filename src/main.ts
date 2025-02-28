da902f15e3c9881e7c9757a6f05fa4ef22f7c4e6
ranula.gamage <ranula.g@insighture.com>
2025-02-28 08:25:48 +0530

    feat: set up vue-router and update alias mapping for cleaner imports

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import router from "./router";
 import App from "./App.vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
-import "./style.css";
 
 const app = createApp(App);
 
@@ -11,5 +11,7 @@ const pinia = createPinia();
 app.use(pinia);
 
 app.use(ElementPlus);
+
+app.use(router);
 
 app.mount("#app");
