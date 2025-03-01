dd6b39c75214756f34d1cb0c268bdeaeb7739838
ranula.gamage <ranula.g@insighture.com>
2025-03-01 08:16:03 +0530

    feat: improved  mobile responsive elements

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,11 @@
   <div class="p-5">
     <div class="max-w-7xl mx-auto">
       <!-- Header Section -->
-      <div class="flex w-full items-center justify-center space-x-8 mt-6">
-        <filter-bar />
+      <div
+        class="flex flex-row sm:flex-col md:flex-row items-center justify-center mt-6 space-x-8"
+      >
+        <filter-bar class="w-full sm:w-auto" />
         <el-button type="success" @click="openBiddings">
-          <!-- <el-icon class="mr-1">
-            <arrow-left-bold />
-          </el-icon> -->
           View Biddings
         </el-button>
       </div>
@@ -23,7 +22,10 @@
           <el-col
             v-for="vehicle in store.filteredVehicles"
             :key="vehicle.id"
-            :span="8"
+            :span="24"
+            :sm="12"
+            :md="8"
+            class="mb-4"
           >
             <vehicle-card :vehicle="vehicle" />
           </el-col>
@@ -47,8 +49,6 @@ const store = useBidStore();
 const loading = ref(true);
 const drawerVisible = ref(false);
 
-
-//todo - mobile responsive 
 onMounted(async () => {
   if (!store.vehicles.length) {
     await store.initializeData();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default  {
+export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
@@ -7,25 +7,31 @@ export default  {
       fontFamily: {
         sans: ["system-ui", "Avenir", "Helvetica", "Arial", "sans-serif"],
       },
-      // colors: {
-      //   background: {
-      //     DEFAULT: "#242424", 
-      //     light: "#ffffff", 
-      //   },
-      //   text: {
-      //     DEFAULT: "rgba(255, 255, 255, 0.87)",
-      //     light: "#213547", 
-      //   },
-      //   link: {
-      //     DEFAULT: "#646cff",
-      //     hover: "#535bf2",
-      //     lightHover: "#747bff",
-      //   },
-      //   button: {
-      //     DEFAULT: "#1a1a1a",
-      //     light: "#f9f9f9",
-      //   },
-      // },
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+      colors: {
+        background: {
+          DEFAULT: "#242424",
+          light: "#ffffff",
+        },
+        text: {
+          DEFAULT: "rgba(255, 255, 255, 0.87)",
+          light: "#213547",
+        },
+        link: {
+          DEFAULT: "#646cff",
+          hover: "#535bf2",
+          lightHover: "#747bff",
+        },
+        button: {
+          DEFAULT: "#1a1a1a",
+          light: "#f9f9f9",
+        },
+      },
     },
   },
   plugins: [],
